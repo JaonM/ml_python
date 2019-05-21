955a97289bad2542d22ceeb8e8b2b1c6d46ef3fe
maqiang <739686359@qq.com>
2019-05-21 21:30:43 +0800

    create knn module

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # learn_ml
 Implementation of machine learning&amp;deep learning algorithms
+
+
+
+## 目录结构
+
+- learn_ml	模型代码
+- tests    代码测试及知识点摘要 
+
---
+++ learn_ml/neighbour/kdtree.py
@@ -0,0 +1,48 @@
+"""
+K-D Tree Implementation
+"""
+import numpy as np
+
+
+class KDTree(object):
+    """
+    Similar to binary search tree
+
+    A data structure to split the data space
+
+    Attributes:
+        root: root node of the tree
+        dim: data dimension
+    """
+
+    def __init__(self, root=None, dim=0):
+        self.root = root
+        self.dim = dim
+
+    def insert(self, data, cd):
+        if isinstance(data, (list, set, tuple)):
+            dim = len(data)
+        elif isinstance(data, np.ndarray):
+            dim = data.shape[1]
+        else:
+            raise TypeError('not support type', type(data))
+        if dim != self.dim:
+            raise ValueError('not legal dimension,plz check')
+
+
+class KDNode(object):
+    """
+    KDTree node
+
+    Attributes:
+        data: data entity,can be any dimension
+        cd: cutting dimension
+        left_child: left child node
+        right_child: right child node
+    """
+
+    def __init__(self, data, cd, left_child=None, right_child=None):
+        self.data = data
+        self.cd = cd
+        self.left_child = left_child
+        self.right_child = right_child
