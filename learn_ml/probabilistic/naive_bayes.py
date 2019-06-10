faf85403da056ea6c16a8335630a645214c00c00
maqiang <739686359@qq.com>
2019-06-10 23:45:46 +0800

    update nb predict

--- learn_ml/probabilistic/naive_bayes.py
+++ learn_ml/probabilistic/naive_bayes.py
@@ -2,6 +2,7 @@
 Naive Bayes Model
 """
 import numpy as np
+import math
 from learn_ml.utility import save_divide
 
 
@@ -12,8 +13,8 @@ class NaiveBayes(object):
         dim:
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, smooth_strategy='laplace'):
+        self.smooth_strategy = smooth_strategy
 
     def fit(self, X, y):
         if not isinstance(X, np.ndarray):
@@ -22,7 +23,7 @@ class NaiveBayes(object):
         # save y probability
         self.y_prob = dict()
         for _y, count in np.unique(y, return_counts=True):
-            self.y_prob[_y] = count/len(y)
+            self.y_prob[_y] = count / len(y)
         self.feature_map = dict()
         for column in range(X.shape[1]):
             feature_map = dict()
@@ -43,8 +44,45 @@ class NaiveBayes(object):
                 for key, value in p_x_y.items():
                     for _key, _value in value.items():
                         p_x_y[key][_key] = _value / len(y)
-                feature_map[column] = p_x_y
+                feature_map['prob'] = p_x_y
             self.feature_map[column] = feature_map
+
+    @staticmethod
+    def gaussion_probality_dense(x, mean, std):
+        return 1 / (math.sqrt(2 * math.pi * std ** 2)) * math.exp((-1 * (x - mean) ** 2) / (2 * std ** 2))
+
+    def predict(self, X):
+        """
+
+        :param X: require 2-d array
+        :return:
+        """
+        if X.shape[1] != self.dim:
+            raise ValueError('Data structure error,require 2-d array')
+        ret = []
+        for sample in X:
+            target, max_p = None, None
+            for y in self.y_prob.keys():
+                p = 1
+                for i in range(self.dim):
+                    if self.feature_map[i]['type'] == 'continuous':
+                        p = p * self.gaussion_probality_dense(sample[i], self.feature_map[i]['mean'],
+                                                              self.feature_map[i]['std'])
+                    else:
+                        # TODO add smooth method
+                        if y in self.feature_map[i]['prob'][sample[i]].keys():
+                            p = p * self.feature_map[i]['prob'][sample[i]][y]
+                        else:
+                            # laplace smooth
+                            p = p * (1 / len(self.feature_map[i]['prob'].keys()))
+                if not target and not max_p:
+                    target = y
+                    max_p = p
+                elif p > max_p:
+                    max_p = p
+                    target = y
+            ret.append(target)
+        return ret
 
     @staticmethod
     def is_continuous(x, con_ratio=0.95):
@@ -55,7 +93,6 @@ class NaiveBayes(object):
         :return:
         """
         _unique = np.unique(x)
-        print(type(x[0]))
         if len(_unique) / len(x) >= con_ratio and type(x[0]) != str:
             return True
         else:
@@ -71,3 +108,4 @@ if __name__ == '__main__':
     clf.fit(x, y)
     print(clf.feature_map)
     print(clf.y_prob)
+    print(clf.predict(np.array([[1.1, 'asd', 3]], dtype=object)))
