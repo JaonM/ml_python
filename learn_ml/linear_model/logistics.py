72aa5e20f0a2ecc23f2e2408f220d64a39e65f74
maqiang <739686359@qq.com>
2019-07-08 23:25:44 +0800

    init logistics regression

---
+++ learn_ml/linear_model/logistics.py
@@ -0,0 +1,19 @@
+"""
+逻辑回归模型
+
+TODO
+    1. 支持不同的优化器
+    2. 支持多种正则化项
+    3. 支持多分类
+    4. 支持并行
+"""
+
+
+class LogisticsRegression(object):
+    """
+    Logistics Regression Model
+
+    Parameters:
+
+    """
+    pass
