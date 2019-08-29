74fc6873f740d7fa29df0427adb112cb89982fc6
maqiang <739686359@qq.com>
2019-08-29 23:53:33 +0800

    complete mini batch gradient descent

--- learn_ml/linear_model/optimizer.py
+++ learn_ml/linear_model/optimizer.py
@@ -98,7 +98,52 @@ def min_batch_gradient_descent(X, y, n_iter, coef, bias, batch_size, lr, lr_stra
     :param patience:
     :return:
     """
-    pass
+    act_iter = 0  # actual number of iteration
+
+    # compute initial loss
+    y_pred = np.apply_along_axis(sigmoid, 1, X, coef=coef, bias=bias)
+    best_loss, last_loss = cross_entropy(y_pred, y), cross_entropy(y_pred, y)
+    iter_loss_no_change = 0
+    losses = []
+
+    for _ in range(n_iter):
+        act_iter += 1
+        p = np.random.permutation(X)
+        X, y = X[p], y[p]
+        for X_bat, y_bat in to_batches(X, y, batch_size):
+            # update coef
+            # compute gradient
+            h_X = np.apply_along_axis(sigmoid, 1, X_bat, coef=coef, bias=bias)
+            tmp = np.matmul(X_bat.T, h_X - y_bat) / len(X_bat)
+            # g = eta * np.sum(tmp, axis=0) / len(X)
+            g = lr * tmp
+            if C and not alpha:  # l2 penalty
+                coef = coef - g - C / len(X_bat) * coef
+            elif alpha and not C:  # l1 penalty
+                coef = coef - g + alpha / len(X_bat) * (-1 * coef)
+            else:
+                coef = coef - g
+            if bias:
+                tmp = h_X - y_bat
+                g = lr * np.sum(tmp, axis=0) / len(X_bat)
+                bias = bias - g.item()
+
+        y_pred = np.apply_along_axis(sigmoid, 1, X, coef=coef, bias=bias)
+        cur_loss = cross_entropy(y_pred, y)
+        losses.append(cur_loss)
+        if cur_loss < last_loss:
+            iter_loss_no_change = 0
+        last_loss = cur_loss
+        if cur_loss <= best_loss:
+            best_loss = cur_loss
+        if cur_loss > best_loss + tol:
+            iter_loss_no_change += 1
+            if iter_loss_no_change > patient:
+                break
+
+        # TODO learning update strategy
+        if lr_strategy != 'fix':
+            pass
 
 
 def to_batches(X, y, batch_size):
